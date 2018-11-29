2f383db8f63da58d61aa9dfddeede90d4ecc53a8
toanGa <trantoan20122582@gmail.com>
2018-11-29 15:38:03 +0700

    init git

---
+++ USB dev/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxReceive = new System.Windows.Forms.RichTextBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.buttonInit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBoxReceive
+            // 
+            this.richTextBoxReceive.Location = new System.Drawing.Point(12, 42);
+            this.richTextBoxReceive.Name = "richTextBoxReceive";
+            this.richTextBoxReceive.Size = new System.Drawing.Size(511, 148);
+            this.richTextBoxReceive.TabIndex = 0;
+            this.richTextBoxReceive.Text = "";
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(208, 212);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // textBoxSend
+            // 
+            this.textBoxSend.Location = new System.Drawing.Point(12, 279);
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(511, 20);
+            this.textBoxSend.TabIndex = 2;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(13, 305);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 3;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(109, 212);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(75, 23);
+            this.buttonRead.TabIndex = 4;
+            this.buttonRead.Text = "Read";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
+            // 
+            // buttonInit
+            // 
+            this.buttonInit.Location = new System.Drawing.Point(13, 212);
+            this.buttonInit.Name = "buttonInit";
+            this.buttonInit.Size = new System.Drawing.Size(75, 23);
+            this.buttonInit.TabIndex = 5;
+            this.buttonInit.Text = "Init";
+            this.buttonInit.UseVisualStyleBackColor = true;
+            this.buttonInit.Click += new System.EventHandler(this.Init_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonInit);
+            this.Controls.Add(this.buttonRead);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxSend);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.richTextBoxReceive);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxReceive;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.Button buttonInit;
+    }
+}
+
---
+++ USB dev/Form1.cs
@@ -0,0 +1,403 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibUsbDotNet;
+using LibUsbDotNet.Info;
+using LibUsbDotNet.Main;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+using LibUsbDotNet.DeviceNotify;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public DateTime LastDataEventDate = DateTime.Now;
+        public UsbDevice MyUsbDevice;
+        public UsbEndpointReader reader;
+        public UsbEndpointWriter writer;
+
+        #region SET YOUR USB Vendor and Product ID!
+
+        public UsbDeviceFinder MyUsbFinder = new UsbDeviceFinder(1155, 22352);
+        public IDeviceNotifier UsbDeviceNotifier = DeviceNotifier.OpenDeviceNotifier();
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void USBDeviceDeinit()
+        {
+            ErrorCode ec = ErrorCode.None;
+            if (MyUsbDevice != null && !MyUsbDevice.IsOpen)
+            {
+                try
+                {
+                    MyUsbDevice = UsbDevice.OpenUsbDevice(MyUsbFinder);
+
+                    // If the device is open and ready
+                    if (MyUsbDevice == null) throw new Exception("Device Not Found.");
+                    IUsbDevice wholeUsbDevice = MyUsbDevice as IUsbDevice;
+                    if (!ReferenceEquals(wholeUsbDevice, null))
+                    {
+                        // This is a "whole" USB device. Before it can be used, 
+                        // the desired configuration and interface must be selected.
+
+                        // Select config #1
+                        wholeUsbDevice.SetConfiguration(1);
+
+                        // Claim interface #0.
+                        wholeUsbDevice.ClaimInterface(0);
+                    }
+
+                    // open read endpoint 1.
+                    reader = MyUsbDevice.OpenEndpointReader(ReadEndpointID.Ep01);
+
+                    // open write endpoint 1.
+                    writer = MyUsbDevice.OpenEndpointWriter(WriteEndpointID.Ep01);
+
+                    reader.DataReceived += (OnRxEndPointData);
+                    reader.DataReceivedEnabled = true;
+
+                    Thread.Sleep(100);
+
+                    // Always disable and unhook event when done.
+                    //reader.DataReceivedEnabled = false;
+                    //reader.DataReceived -= (OnRxEndPointData);
+
+                    Console.WriteLine("\r\nDone!\r\n");
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        void readOnly()
+        {
+            ErrorCode ec = ErrorCode.None;
+
+            try
+            {
+                // Find and open the usb device.
+                MyUsbDevice = UsbDevice.OpenUsbDevice(MyUsbFinder);
+
+                // If the device is open and ready
+                if (MyUsbDevice == null) throw new Exception("Device Not Found.");
+
+                // If this is a "whole" usb device (libusb-win32, linux libusb-1.0)
+                // it exposes an IUsbDevice interface. If not (WinUSB) the 
+                // 'wholeUsbDevice' variable will be null indicating this is 
+                // an interface of a device; it does not require or support 
+                // configuration and interface selection.
+                IUsbDevice wholeUsbDevice = MyUsbDevice as IUsbDevice;
+                if (!ReferenceEquals(wholeUsbDevice, null))
+                {
+                    // This is a "whole" USB device. Before it can be used, 
+                    // the desired configuration and interface must be selected.
+
+                    // Select config #1
+                    wholeUsbDevice.SetConfiguration(1);
+
+                    // Claim interface #0.
+                    wholeUsbDevice.ClaimInterface(0);
+                }
+
+                // open read endpoint 1.
+                reader = MyUsbDevice.OpenEndpointReader(ReadEndpointID.Ep01);
+
+
+                byte[] readBuffer = new byte[1024];
+                while (ec == ErrorCode.None)
+                {
+                    int bytesRead;
+
+                    // If the device hasn't sent data in the last 5 seconds,
+                    // a timeout error (ec = IoTimedOut) will occur. 
+                    ec = reader.Read(readBuffer, 5000, out bytesRead);
+
+                    if (bytesRead == 0) throw new Exception(string.Format("{0}:No more bytes!", ec));
+                    Console.WriteLine("{0} bytes read", bytesRead);
+
+                    // Write that output to the console.
+                    Console.Write(Encoding.Default.GetString(readBuffer, 0, bytesRead));
+                }
+
+                Console.WriteLine("\r\nDone!\r\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine((ec != ErrorCode.None ? ec + ":" : String.Empty) + ex.Message);
+            }
+            finally
+            {
+                if (MyUsbDevice != null)
+                {
+                    if (MyUsbDevice.IsOpen)
+                    {
+                        // If this is a "whole" usb device (libusb-win32, linux libusb-1.0)
+                        // it exposes an IUsbDevice interface. If not (WinUSB) the 
+                        // 'wholeUsbDevice' variable will be null indicating this is 
+                        // an interface of a device; it does not require or support 
+                        // configuration and interface selection.
+                        IUsbDevice wholeUsbDevice = MyUsbDevice as IUsbDevice;
+                        if (!ReferenceEquals(wholeUsbDevice, null))
+                        {
+                            // Release interface #0.
+                            wholeUsbDevice.ReleaseInterface(0);
+                        }
+
+                        MyUsbDevice.Close();
+                    }
+                    MyUsbDevice = null;
+
+                    // Free usb resources
+                    UsbDevice.Exit();
+
+                }
+            }
+        }
+
+        void readWriteERventDrivent()
+        {
+            ErrorCode ec = ErrorCode.None;
+
+            try
+            {
+                // Find and open the usb device.
+                MyUsbDevice = UsbDevice.OpenUsbDevice(MyUsbFinder);
+
+                // If the device is open and ready
+                if (MyUsbDevice == null) throw new Exception("Device Not Found.");
+
+
+                // If this is a "whole" usb device (libusb-win32, linux libusb-1.0)
+                // it exposes an IUsbDevice interface. If not (WinUSB) the 
+                // 'wholeUsbDevice' variable will be null indicating this is 
+                // an interface of a device; it does not require or support 
+                // configuration and interface selection.
+                IUsbDevice wholeUsbDevice = MyUsbDevice as IUsbDevice;
+                if (!ReferenceEquals(wholeUsbDevice, null))
+                {
+                    // This is a "whole" USB device. Before it can be used, 
+                    // the desired configuration and interface must be selected.
+
+                    // Select config #1
+                    wholeUsbDevice.SetConfiguration(1);
+
+                    // Claim interface #0.
+                    wholeUsbDevice.ClaimInterface(0);
+                }
+
+                // open read endpoint 1.
+                reader = MyUsbDevice.OpenEndpointReader(ReadEndpointID.Ep01);
+                reader.ReadThreadPriority = System.Threading.ThreadPriority.Highest;
+                reader.ReadBufferSize = 64;
+                // open write endpoint 1.
+                writer = MyUsbDevice.OpenEndpointWriter(WriteEndpointID.Ep01);
+
+                // Remove the exepath/startup filename text from the begining of the CommandLine.
+                string cmdLine = Regex.Replace(
+                    Environment.CommandLine, "^\".+?\"^.*? |^.*? ", "", RegexOptions.Singleline);
+
+                if (!String.IsNullOrEmpty(cmdLine))
+                {
+                    reader.DataReceived += (OnRxEndPointData);
+                    reader.DataReceivedEnabled = true;
+
+                    int bytesWritten;
+                    ec = writer.Write(Encoding.Default.GetBytes(cmdLine), 2000, out bytesWritten);
+                    if (ec != ErrorCode.None) throw new Exception(UsbDevice.LastErrorString);
+
+                    LastDataEventDate = DateTime.Now;
+                    while ((DateTime.Now - LastDataEventDate).TotalMilliseconds < 100)
+                    {
+                    }
+
+                    // Always disable and unhook event when done.
+                    //reader.DataReceivedEnabled = false;
+                    //reader.DataReceived -= (OnRxEndPointData);
+
+                    Console.WriteLine("\r\nDone!\r\n");
+                }
+                else
+                    throw new Exception("Nothing to do.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine((ec != ErrorCode.None ? ec + ":" : String.Empty) + ex.Message);
+            }
+            finally
+            {
+                //if (MyUsbDevice != null)
+                //{
+                //    if (MyUsbDevice.IsOpen)
+                //    {
+                //        // If this is a "whole" usb device (libusb-win32, linux libusb-1.0)
+                //        // it exposes an IUsbDevice interface. If not (WinUSB) the 
+                //        // 'wholeUsbDevice' variable will be null indicating this is 
+                //        // an interface of a device; it does not require or support 
+                //        // configuration and interface selection.
+                //        IUsbDevice wholeUsbDevice = MyUsbDevice as IUsbDevice;
+                //        if (!ReferenceEquals(wholeUsbDevice, null))
+                //        {
+                //            // Release interface #0.
+                //            wholeUsbDevice.ReleaseInterface(0);
+                //        }
+                //        MyUsbDevice.Close();
+                //    }
+                //}
+                //MyUsbDevice = null;
+
+                // Free usb resources
+                //UsbDevice.Exit();
+            }
+        }
+
+        private void OnDeviceNotifyEvent(object sender, DeviceNotifyEventArgs e)
+        {
+            // A Device system-level event has occured
+            if(e.EventType == EventType.DeviceArrival)
+            {
+                richTextBoxReceive.AppendText("Device arrival\n");
+                USBDeviceDeinit();
+            }
+            else if(e.EventType == EventType.DeviceRemoveComplete)
+            {
+                richTextBoxReceive.AppendText("Device remove complete\n");
+            }
+            else
+            {
+                richTextBoxReceive.AppendText("Deveice Event: " + e.EventType.ToString() + "\n");
+            }
+            richTextBoxReceive.AppendText(e.ToString() + "\n");
+        }
+
+        private  void OnRxEndPointData(object sender, EndpointDataEventArgs e)
+        {
+
+            LastDataEventDate = DateTime.Now;
+
+            if (InvokeRequired)
+            {
+                BeginInvoke((MethodInvoker)delegate
+                {
+                    try
+                    {
+                        richTextBoxReceive.AppendText(Encoding.Default.GetString(e.Buffer, 0, e.Count) + "\n");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                });
+            }
+            else
+            {
+                richTextBoxReceive.AppendText(Encoding.Default.GetString(e.Buffer, 0, e.Count) + "\n");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int bytesWritten;
+            ErrorCode ec = writer.Write(Encoding.ASCII.GetBytes(textBoxSend.Text), 2000, out bytesWritten);
+            if (ec != ErrorCode.None)
+            {
+                richTextBoxReceive.AppendText("Error send\n");
+            }
+            else
+            {
+                richTextBoxReceive.AppendText("Sent " + bytesWritten + " bytes\n");
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            richTextBoxReceive.Text = "";
+        }
+
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            ErrorCode ecRead;
+            byte[] readBuffer = new byte[1024];
+            string strRead;
+            int bytesRead;
+            ecRead = reader.Read(readBuffer, 1000, out bytesRead);
+            strRead = Encoding.Default.GetString(readBuffer, 0, bytesRead);
+
+            if(bytesRead != 0)
+            {
+                richTextBoxReceive.AppendText(strRead + "\n");
+            }
+            else
+            {
+                richTextBoxReceive.AppendText("No more byte read\n");
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                reader.DataReceivedEnabled = false;
+                reader.DataReceived -= (OnRxEndPointData);
+                reader.Dispose();
+                writer.Dispose();
+
+                if (MyUsbDevice != null)
+                {
+                    if (MyUsbDevice.IsOpen)
+                    {
+                        IUsbDevice wholeUsbDevice = MyUsbDevice as IUsbDevice;
+                        if (!ReferenceEquals(wholeUsbDevice, null))
+                        {
+                            wholeUsbDevice.ReleaseInterface(0);
+                        }
+                        MyUsbDevice.Close();
+
+                    }
+                    MyUsbDevice = null;
+                    UsbDevice.Exit();
+                }
+            
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void Init_Click(object sender, EventArgs e)
+        {
+            // Hook the device notifier event
+            UsbDeviceNotifier.OnDeviceNotify += OnDeviceNotifyEvent;
+
+            readWriteERventDrivent();
+            //readOnly();
+
+            if(MyUsbDevice != null && MyUsbDevice.IsOpen)
+            {
+                buttonInit.Enabled = false;
+                richTextBoxReceive.AppendText("Init USB success\n");
+            }
+            else
+            {
+                richTextBoxReceive.AppendText("Init USB Fail\n");
+            }
+        }
+    }
+}
